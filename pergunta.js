d0193cd8d6fc0cbd61582baf3f28d09804fd8990
santos01kaua <caua.santos.silva17@escola.pr.gov.br>
2024-09-30 13:55:20 -0300

    30/09

---
+++ pergunta.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Programação',
+    'oque é python?',
+    'o python é uma linguagem de programação'
+)
+
+criacartão(
+    'geografia',
+    'qual a capital a frança?',
+    'a capital da frança é paris'
+)
+
+criaCartao(
+    'programação',
+    'o que é um furacão?',
+    'uma funçao é um bloco de código que eecuta alguma tarefa'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'como se diz oi em inglês?',
+    'oi em inglẽs é hi (rai)'
+)
+
+
+
+
